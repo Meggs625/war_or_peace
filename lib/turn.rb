0da777eb788342a08141790e7cadc3227d0f94d1
Megan McBride <megan.mcbride@colorado.edu>
2024-11-20 17:08:25 -0700

    Beginning to build out the Turn class WIP

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+class Turn
+  attr_reader :player_1, :player_2, :spoils_of_war
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @spoils_of_war = []
+  end
+
+end
